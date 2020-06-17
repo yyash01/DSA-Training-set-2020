fe53e4f1660b5d8c4081eeb59e6ca28262e4ca25
yyash01 <yjain3.436@gmail.com>
2020-06-17 17:09:23 +0530

    modified fileopen.c-with-useful-comments

--- training_set-03/tr3_task2/fileopen.c
+++ training_set-03/tr3_task2/fileopen.c
@@ -6,7 +6,9 @@ BATCH - CP(LAB-03) WEDNESDAY AFTERNOON
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-
+/*
+getc() returns EOF when end of file is reached. getc() also returns EOF when it fails. So, only comparing the value returned by getc() with EOF is not sufficient to check for actual end of file. To solve this problem, C provides feof() which returns non-zero value only if end of file has reached, otherwise it returns 0.
+*/
 void main()
 {
     FILE *fp;
@@ -14,7 +16,7 @@ void main()
     assert(fp != NULL);
     char ch;
     ch = fgetc(fp);
-    while (!feof(fp))
+    while (!feof(fp))//means end of file is not reached jb tk ye while loop chlana hai
     {
         printf("%c", ch);
         ch = fgetc(fp);
